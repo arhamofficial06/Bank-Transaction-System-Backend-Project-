d90adca7eb9458ad8bc5476aa7469b786984f084
Muhammad Arham <arhamofficial28@gmail.com>
2026-03-17 15:02:07 +0500

    17th March - Server connected ko Database

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const app = require("./src/app");
 const port = process.env.PORT;
+const connectToDB = require("./src/config/db");
+
+connectToDB();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
