71c93f98678ddca8fa8f43e965e89f8057f2936d
Muhammad Arham <arhamofficial28@gmail.com>
2026-03-17 16:30:22 +0500

    17th March - Add Register API & its logic

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ function connectToDB() {
     .connect(process.env.MONGO_URI)
     .then(() => console.log("Server is connected to DB"))
     .catch((err) => {
-      console.log("Error connecting to DB", err);
+      console.log("Error connecting to DB");
       process.exit(1);
     });
 }
