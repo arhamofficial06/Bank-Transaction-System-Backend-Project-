e2f1dd65b2755f70cb7ac10e28192b600e10f19a
Muhammad Arham <arhamofficial28@gmail.com>
2026-03-18 18:19:32 +0500

    18th March - Add Create Account Route

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
-const authRouter = require("./routes/auth.routes");
 const cookieParser = require("cookie-parser");
+
 
 app.use(express.json());
 app.use(cookieParser());
 
+/**
+ * - Routes Required
+ */
+const authRouter = require("./routes/auth.routes");
+const accountRouter = require("./routes/account.routes");
+
+/**
+ * - Use Routes
+ */
 app.use("/api/auth", authRouter);
+app.use("/api/accounts", accountRouter);
 
 module.exports = app;
